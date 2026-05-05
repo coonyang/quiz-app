7886e39b3551713915ad6e176f80ffdb28c44145
Hajun Jeon <gkwns1231@naver.com>
2026-05-06 05:41:20 +0900

    문제를 직접 넣어 한문제씩 풀 수 있는 코드 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,58 @@
+"use client";
+
 import { questions } from "./data/questions";
+import { useState } from "react";
 
 export default function Home() {
+  const [quizQuestions, setQuizQuestions] = useState(questions);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
+
+  const startQuiz = () => {
+    const shuffled = [...questions].sort(() => Math.random() - 0.5);
+    const selected = shuffled.slice(0, 10);
+
+    setQuizQuestions(selected);
+    setCurrentIndex(0);
+    setIsStarted(true);
+  };
+
+  const currentQuestion = quizQuestions[currentIndex];
+
+  const selectChoice = (choiceIndex: number) => {
+    const answer = currentQuestion.answerIndex;
+    if (choiceIndex == answer) {
+      alert("정답입니다.");
+      if (currentIndex < quizQuestions.length - 1) {
+        setCurrentIndex((prev) => prev + 1);
+      } else {
+        alert("문제 끝!!");
+        setIsStarted(false);
+      }
+    } else {
+      alert("땡!!");
+    }
+  };
+
   return (
     <main>
-      <h1>퀴즈 목록</h1>
+      {!isStarted && <button onClick={startQuiz}>시작하기</button>}
 
-      {questions.map((item) => (
-        <section key={item.id}>
-          <h2>{item.question}</h2>
+      {isStarted && currentQuestion && (
+        <section>
+          <p>
+            {currentIndex + 1} / {quizQuestions.length}
+          </p>
 
-          {item.choices.map((choice, index) => (
-            <button key={choice}>
+          <h2>{currentQuestion.question}</h2>
+
+          {currentQuestion.choices.map((choice, index) => (
+            <button onClick={() => selectChoice(index)}>
               {index + 1}. {choice}
             </button>
           ))}
         </section>
-      ))}
+      )}
     </main>
   );
 }
