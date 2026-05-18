45fb52ccf49f3d8b8cfa825e6fd96c14c8cf8419
Hajun Jeon <gkwns1231@naver.com>
2026-05-19 05:08:35 +0900

    퀴즈 업데이트

--- app/data/quizSets.ts
+++ app/data/quizSets.ts
@@ -10,20 +10,20 @@ export const quizSets = [
         id: 1,
         question: "산소의 원소기호는 무엇일까요?",
         choices: ["O", "Q", "S", "B"],
-        answerIndex: 1,
+        answerIndex: 0,
       },
       {
         id: 2,
         question: "구리의 원소기호는 무엇일까요?",
         choices: ["Cu", "An", "Dn", "Pu"],
-        answerIndex: 1,
+        answerIndex: 0,
       },
       {
         id: 3,
         question:
           "지구 대기 성분 중 가장 많은 부피를 차지하는 기체는 무엇일까요?",
         choices: ["산소", "질소", "이산화탄소", "일산화탄소"],
-        answerIndex: 2,
+        answerIndex: 1,
       },
       {
         id: 4,
@@ -34,40 +34,33 @@ export const quizSets = [
           "늘어난다",
           "온도랑 관계가 없다",
         ],
-        answerIndex: 3,
+        answerIndex: 2,
       },
       {
         id: 5,
         question:
           "우리 몸에서 혈액을 온몸으로 순환시키는 펌프 역할을 하는 기관은 어디일까요?",
         choices: ["심장", "간", "대장", "뇌"],
-        answerIndex: 1,
-      },
-      {
-        id: 5,
-        question:
-          "우리 몸에서 혈액을 온몸으로 순환시키는 펌프 역할을 하는 기관은 어디일까요?",
-        choices: ["심장", "간", "대장", "뇌"],
-        answerIndex: 1,
+        answerIndex: 0,
       },
       {
         id: 6,
         question: "다음 보기중 태양계에서 가장 크기가 큰 행성은 무엇일까요?",
         choices: ["토성", "금성", "지구", "목성"],
-        answerIndex: 4,
+        answerIndex: 3,
       },
       {
         id: 7,
         question:
           "식물이 태양빛을 이용해 에너지를 만드는 과정을 무엇이라할까요?",
         choices: ["증산작용", "광합성", "식물의 호흡 제 1형", "삼투압"],
-        answerIndex: 2,
+        answerIndex: 1,
       },
       {
         id: 8,
         question: "사람이 물에 뜨는 이유는 무슨 힘이 작용해서 일까요?",
         choices: ["부력", "중력", "달력", "바다거북이가 올려줌"],
-        answerIndex: 1,
+        answerIndex: 0,
       },
       {
         id: 9,
@@ -79,21 +72,21 @@ export const quizSets = [
           "관성의 법칙",
           "작용 반작용의 법칙",
         ],
-        answerIndex: 3,
+        answerIndex: 2,
       },
       {
         id: 10,
         question:
           "빛이 서로 다른 매질의 경계면을 지나갈 때 진행 방향이 꺾이는 현상은?",
         choices: ["반사", "회절", "중요한건 꺾이지 않는 마음", "굴절"],
-        answerIndex: 4,
+        answerIndex: 3,
       },
       {
         id: 11,
         question:
           "지진이 발생했을 때 지표면에서 가장 먼더 도달하는 종파는 무엇일까요?",
         choices: ["L파", "S파", "P파", "A파"],
-        answerIndex: 3,
+        answerIndex: 2,
       },
     ],
   },
@@ -108,7 +101,7 @@ export const quizSets = [
         id: 1,
         question: "2 + 3은?",
         choices: ["4", "5", "6", "7"],
-        answerIndex: 2,
+        answerIndex: 1,
       },
     ],
   },
@@ -124,21 +117,21 @@ export const quizSets = [
         question:
           "인터 프리터가 코드를 실행하기 이전 단계에서 함수, 변수, 클래스, 임포트의 선언문을 해당 범위의 최상단으로 이동시기는 과정은?",
         choices: ["호이스팅", "슈팅스타", "업로드", "업데이트"],
-        answerIndex: 1,
+        answerIndex: 0,
       },
       {
         id: 2,
         question:
           "개발을 위해 필요한 기능을 모아놓은 소프트웨어를 뭐라고 할까요?",
         choices: ["프레임워크", "리액트", "라이브러리", "자바스크립트"],
-        answerIndex: 3,
+        answerIndex: 2,
       },
       {
         id: 3,
         question:
           "Hypertext Transfer Protocol의 준말로, 웹에서 데이터를 주고받기 위한 프로토콜을 뭐라고 할까요?",
         choices: ["http", "htp", "hp", "https"],
-        answerIndex: 1,
+        answerIndex: 0,
       },
     ],
   },
@@ -153,7 +146,7 @@ export const quizSets = [
         id: 1,
         question: "야옹 이라고 우는 동물 이름은?",
         choices: ["고양이", "개", "양", "햄스터"],
-        answerIndex: 1,
+        answerIndex: 0,
       },
     ],
   },
